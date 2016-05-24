88a5faafa40cd663d44cb53751613af2298395e8
jerusalemdax <jerusalemdax@gmail.com>
2016-05-24 20:32:28 +0800

    fix warning

--- Assets/Core/AssetBundleLoadOperation.cs
+++ Assets/Core/AssetBundleLoadOperation.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 namespace AssetBundles
 {
@@ -66,15 +67,13 @@ namespace AssetBundles
 	{
 		protected string 				m_AssetBundleName;
 		protected string 				m_LevelName;
-		protected bool 					m_IsAdditive;
-		protected string 				m_DownloadingError;
+	    protected string 				m_DownloadingError;
 		protected AsyncOperation		m_Request;
 
 		public AssetBundleLoadLevelOperation (string assetbundleName, string levelName, bool isAdditive)
 		{
 			m_AssetBundleName = assetbundleName;
 			m_LevelName = levelName;
-			m_IsAdditive = isAdditive;
 		}
 
 		public override bool Update ()
@@ -85,10 +84,7 @@ namespace AssetBundles
 			LoadedAssetBundle bundle = AssetBundleManager.GetLoadedAssetBundle (m_AssetBundleName, out m_DownloadingError);
 			if (bundle != null)
 			{
-				if (m_IsAdditive)
-					m_Request = Application.LoadLevelAdditiveAsync (m_LevelName);
-				else
-					m_Request = Application.LoadLevelAsync (m_LevelName);
+			    SceneManager.LoadSceneAsync(m_LevelName);
 				return false;
 			}
 			else
