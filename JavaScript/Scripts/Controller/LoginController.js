20142fe807f329ca57fcee1874ac6ef05a07d9e2
jerusalemdax <jerusalemdax@gmail.com>
2016-07-06 15:23:00 +0800

    增加显示登陆面板

--- JavaScript/Scripts/Controller/LoginController.js
+++ JavaScript/Scripts/Controller/LoginController.js
@@ -3,9 +3,11 @@ var LoginController = {
     definition: {
         Start: function(){
             print("LoginController Start");
+			UIManager.get_Instance().ShowPanel("LoginPanel");
         },
 		OnDestroy: function(){
 			print("LoginController OnDestroy");
+			UIManager.get_Instance().ClosePanel("LoginPanel");
 		}
     }
 };
